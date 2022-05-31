ac4d9e76aece26bbe4e46c95395314ee1d2a38da
brskramnco <104290109+brskramnco@users.noreply.github.com>
2022-05-31 22:13:19 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,63 @@
+newFunction();
 
+function newFunction() {
+    window.sr = ScrollReveal();
+
+    sr.reveal('anime-left', {
+        origin: 'left',
+        duration: 1000,
+        distance: '25ren',
+        delay: 300
+    });
+
+    sr.reveal('anime-right', {
+        origin: 'right',
+        duration: 1000,
+        distance: '25ren',
+        delay: 300
+    });
+    sr.reveal('anime-left', {
+        origin: 'left',
+        duration: 1000,
+        distance: '25ren',
+        delay: 300
+    });
+
+    sr.reveal('anime-right', {
+        origin: 'right',
+        duration: 1000,
+        distance: '25ren',
+        delay: 300
+    });
+    
+    sr.reveal('anime-up', {
+        origin: 'up',
+        duration: 1000,
+        distance: '25ren',
+        delay: 600
+    });
+
+    sr.reveal('anime-top', {
+        origin: 'top',
+        duration: 1000,
+        distance: '25ren',
+        delay: 600
+    });
+    sr.reveal('anime-up', {
+        origin: 'up',
+        duration: 1000,
+        distance: '25ren',
+        delay: 600
+    }); 
+}
+
+var menuItems=document.getElementById('menuItems');
+menuItems.style.maxHeight="0px";
+ 
+function menuToggle(){
+    if(menuItems.style.maxHeight=="0px"){
+        menuItems.style.maxHeight="200px";
+    }else{
+        menuItems.style.maxHeight="0px"
+    }
+}
